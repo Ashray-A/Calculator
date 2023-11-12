028610cdd1bba89910a5fff1b9a526d4eee138e6
Ashray-A <127750953+Ashray-A@users.noreply.github.com>
2023-11-12 18:36:45 +0530

    update everything

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Calculator
+# Simple Calculator
+
+A basic calculator with HTML, CSS, and JavaScript.
+
+## Description
+
+This project is a simple calculator implementation that allows users to perform basic arithmetic operations, including addition, subtraction, multiplication, and division. The calculator features a user-friendly interface with a display, number buttons, operator buttons, and additional functionalities like backspace, clear, and decimal input.
+
+## Features
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Clear button (C)
+- Backspace button (←)
+- Decimal point input
+- Error handling for division by zero
+- Responsive design
--- script.js
+++ script.js
@@ -1,87 +1,137 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Initialize variables
-    let displayValue = '0';
-    let firstNumber = null;
-    let operator = null;
-  
-    // Get elements
-    const display = document.getElementById('display');
-    const buttons = document.querySelectorAll('button');
-  
-    // Helper function to update display
-    function updateDisplay() {
-      display.textContent = displayValue;
-    }
-  
-    // Helper function to perform basic math operations
-    function operate() {
-      const num1 = parseFloat(firstNumber);
-      const num2 = parseFloat(displayValue);
-  
-      switch (operator) {
-        case 'add':
-          displayValue = (num1 + num2).toString();
-          break;
-        case 'subtract':
-          displayValue = (num1 - num2).toString();
-          break;
-        case 'multiply':
-          displayValue = (num1 * num2).toString();
-          break;
-        case 'divide':
-          if (num2 === 0) {
-            displayValue = 'Error';
-          } else {
-            displayValue = (num1 / num2).toString();
-          }
-          break;
-        default:
-          break;
-      }
-  
-      // Round answers with long decimals
-      displayValue = parseFloat(displayValue).toFixed(10).toString().replace(/\.?0+$/, '');
-  
-      updateDisplay();
-    }
-  
-    // Event listener for button clicks
-    buttons.forEach(button => {
-      button.addEventListener('click', function () {
-        const buttonValue = this.textContent;
-  
-        if (!isNaN(buttonValue) || buttonValue === '.') {
-          // Handle number and decimal button clicks
-          displayValue = displayValue === '0' ? buttonValue : displayValue + buttonValue;
-        } else if (buttonValue === 'C') {
-          // Handle clear button click
-          displayValue = '0';
-          firstNumber = null;
-          operator = null;
-        } else if (buttonValue === '←') {
-          // Handle backspace button click
-          displayValue = displayValue.length === 1 ? '0' : displayValue.slice(0, -1);
-        } else if (buttonValue === '=') {
-          // Handle equals button click
-          if (firstNumber !== null && operator !== null) {
-            operate();
-            firstNumber = null;
-            operator = null;
-          }
-        } else {
-          // Handle operator button click
-          if (firstNumber === null) {
-            firstNumber = displayValue;
-            operator = buttonValue;
-          } else {
-            operate();
-            firstNumber = displayValue;
-            operator = buttonValue;
-          }
-        }
-  
-        updateDisplay();
-      });
-    });
-  });
-  
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equalsBtn')
+const clearButton = document.getElementById('clearBtn')
+const deleteButton = document.getElementById('deleteBtn')
+const pointButton = document.getElementById('pointBtn')
+const lastOperationScreen = document.getElementById('lastOperationScreen')
+const currentOperationScreen = document.getElementById('currentOperationScreen')
+
+window.addEventListener('keydown', handleKeyboardInput)
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+pointButton.addEventListener('click', appendPoint)
+
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+)
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === '0' || shouldResetScreen)
+    resetScreen()
+  currentOperationScreen.textContent += number
+}
+
+function resetScreen() {
+  currentOperationScreen.textContent = ''
+  shouldResetScreen = false
+}
+
+function clear() {
+  currentOperationScreen.textContent = '0'
+  lastOperationScreen.textContent = ''
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+
+function appendPoint() {
+  if (shouldResetScreen) resetScreen()
+  if (currentOperationScreen.textContent === '')
+    currentOperationScreen.textContent = '0'
+  if (currentOperationScreen.textContent.includes('.')) return
+  currentOperationScreen.textContent += '.'
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1)
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate()
+  firstOperand = currentOperationScreen.textContent
+  currentOperation = operator
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
+  shouldResetScreen = true
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  secondOperand = currentOperationScreen.textContent
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  )
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+  currentOperation = null
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000
+}
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key)
+  if (e.key === '.') appendPoint()
+  if (e.key === '=' || e.key === 'Enter') evaluate()
+  if (e.key === 'Backspace') deleteNumber()
+  if (e.key === 'Escape') clear()
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/')
+    setOperation(convertOperator(e.key))
+}
+
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === '/') return '÷'
+  if (keyboardOperator === '*') return '×'
+  if (keyboardOperator === '-') return '−'
+  if (keyboardOperator === '+') return '+'
+}
+
+function add(a, b) {
+  return a + b
+}
+
+function substract(a, b) {
+  return a - b
+}
+
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  return a / b
+}
+
+function operate(operator, a, b) {
+  a = Number(a)
+  b = Number(b)
+  switch (operator) {
+    case '+':
+      return add(a, b)
+    case '−':
+      return substract(a, b)
+    case '×':
+      return multiply(a, b)
+    case '÷':
+      if (b === 0) return null
+      else return divide(a, b)
+    default:
+      return null
+  }
+}
